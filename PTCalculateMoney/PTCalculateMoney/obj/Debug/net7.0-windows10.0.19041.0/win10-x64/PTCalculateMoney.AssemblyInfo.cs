e66ddc89551d78b69afd039671078fb438a6d557
Trinh Bac <trinhgiachang@gmail.com>
2023-10-12 22:36:49 +0700

    update

--- PTCalculateMoney/PTCalculateMoney/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/PTCalculateMoney.AssemblyInfo.cs
+++ PTCalculateMoney/PTCalculateMoney/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/PTCalculateMoney.AssemblyInfo.cs
@@ -11,6 +11,10 @@
 using System;
 using System.Reflection;
 
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "7db20414-ad14-46d0-bf13-34ed63a099ce")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "PTCalculateMoney")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("PTCalculateMoney")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
