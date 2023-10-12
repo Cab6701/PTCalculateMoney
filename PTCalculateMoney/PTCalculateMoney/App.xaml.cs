4254b88926a953dfffb814b7de2a4bf171e0b02c
Trinh Bac <trinhgiachang@gmail.com>
2023-10-12 22:26:52 +0700

    create project

---
+++ PTCalculateMoney/PTCalculateMoney/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PTCalculateMoney
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PTCalculateMoney/PTCalculateMoney/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PTCalculateMoney
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
