941ad671854ce8885c256685f446f0bd9e4e0d64
Trinh Bac <trinhgiachang@gmail.com>
2023-12-01 02:29:52 +0700

    delete .vs folder

--- PTCalculateMoney/PTCalculateMoney/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/PTCalculateMoney.AssemblyInfo.cs
+++ PTCalculateMoney/PTCalculateMoney/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/PTCalculateMoney.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "7db20414-ad14-46d0-bf13-34ed63a099ce")]
 [assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "PTCalculateMoney")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "PhuongThuyGrocery")]
 [assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("PTCalculateMoney")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
